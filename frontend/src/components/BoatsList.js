ee9dbe20d5cdf11780963c15a19e83e73ceb59d1
anotherrandomuser1234 <mark_grech@hotmail.com>
2022-12-08 15:54:25 +0100

    UC3- added the frontend CRUD functionality for boats

--- frontend/src/components/BoatsList.js
+++ frontend/src/components/BoatsList.js
@@ -1,38 +1,60 @@
-import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Button, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material";
 import * as React from "react";
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 
 export default function BoatsList(props) {
+
+    const [name, setName] = React.useState("");
+    const [description, setDescription] = React.useState("");
+
     const navigate = useNavigate();
     const items = props?.boats;
     console.log("🚀 ~ file: BoatsList.js ~ line 9 ~ BoatsList ~ items", items)
-    const noBoatsFound = "No Boats Found";
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleDescriptionChange = (e) => {
+        setDescription(e.target.value);
+    }
 
     const goToBoatDetailPage = (boatId) => {
         navigate(`/boats/${boatId}`);
     }
 
     const handleEdit = async (boat) => {
-        await axios.put(`http://localhost:8080/api/boat/${boat.boatId}`);
+        console.log("🚀 ~ file: BoatsList.js ~ line 28 ~ handleEdit ~ boat", boat)
+        let updatedName = name;
+        let updatedDescription = description;
+
+        if (updatedName === "") updatedName = boat.name;
+        if (updatedDescription === "") updatedDescription = boat.description;
+
+        await axios.put(`http://localhost:8080/api/boat/${boat.id}`, {
+            name: updatedName,
+            description: updatedDescription
+        });
     }
 
     const handleDelete = async (boatId) => {
         // make an axios call to delete the boat
         await axios.delete(`http://localhost:8080/api/boat/${boatId}`);
-        // refresh the page
-        
+        await props.getBoats();
     }
 
     return (
 
-        <div className="">
+        <div className="boatContainer">
+
             <TableContainer>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <Table sx={{ minWidth: 650, marginTop: "4rem" }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
                             <TableCell>Name</TableCell>
                             <TableCell>Description</TableCell>
+                            <TableCell></TableCell>
                             <TableCell></TableCell>
                             <TableCell></TableCell>
                         </TableRow>
@@ -43,19 +65,26 @@ export default function BoatsList(props) {
                                 key={boat.id}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
-                                <TableCell>{boat.name}</TableCell>
-                                <TableCell>{boat.description}</TableCell>
-                                <TableCell>
-                                <Button variant="outlined" color="primary" onClick={() => handleEdit(boat)}>Edit</Button>
+                                <TableCell> 
+                                    <TextField autoFocus margin="dense" id="name" type="text" variant="standard" defaultValue={boat.name} onChange={handleNameChange}/>
+                                </TableCell>
+                                <TableCell class="clickable">
+                                    <TextField autoFocus margin="dense" id="name" type="text" variant="standard" defaultValue={boat.description} onChange={handleDescriptionChange}/>
                                 </TableCell>
                                 <TableCell>
-                                <Button variant="outlined" color="error" onClick={() => handleDelete(boat.id)}>Delete</Button>
+                                    <Button variant="outlined" color="primary" onClick={() => goToBoatDetailPage(boat.id)}>Show Details</Button>
                                 </TableCell>
-                                
+                                <TableCell>
+                                    <Button variant="outlined" color="primary" onClick={() => handleEdit(boat)}>Edit</Button>
+                                </TableCell>
+                                <TableCell>
+                                    <Button variant="outlined" color="error" onClick={() => handleDelete(boat.id)}>Delete</Button>
+                                </TableCell>
+
                             </TableRow>
                         ))}
                     </TableBody>
-                   
+
                 </Table>
             </TableContainer>
         </div>
--- frontend/src/pages/BoatPage.js
+++ frontend/src/pages/BoatPage.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import BoatsList from './../components/BoatsList';
+import { Grid, Button } from '@mui/material';
+import { TextField } from '@mui/material';
 
 export default function BoatPage() {
 
-    const [boats, setBoats] = useState([]);
+    const [boats, setBoats] = useState([]);    
+    const [name, setName] = React.useState([]);
+    const [description, setDescription] = React.useState([]);
 
     const getBoats = async () => {
         try {
@@ -16,16 +20,52 @@ export default function BoatPage() {
         }
     };
 
+    const handleCreate = async () => {
+        await axios.post(`http://localhost:8080/api/boat`, {
+            name: name,
+            description: description
+        });
+
+        await getBoats();
+
+    }
+
     React.useEffect(() => {
         getBoats();
     }, []);
 
     return (
-
         <div className="App">
             <h2>List of Boats</h2>
-            <body className="App-body">
-                {boats.length > 0 ? <BoatsList boats={boats} /> : undefined}
+            <Grid container alignItems="end">
+                    <Grid item xs={4}>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Name"
+                            type="text"
+                            variant="standard"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Description"
+                            type="text"
+                            onChange={(e) => setDescription(e.target.value)}
+                            variant="standard"
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Button variant="outlined" color="primary" onClick={() => handleCreate()}>Create</Button>
+                    </Grid>
+                </Grid>
+            <body className="">
+                {boats.length > 0 ? <BoatsList boats={boats} getBoats={getBoats} /> : undefined}
             </body>
         </div>
     );
